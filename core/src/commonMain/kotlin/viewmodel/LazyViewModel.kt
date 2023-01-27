3dc72dea3f1786bd555589c15e3992ab9a1bf324
andylamax <andylamax@programmer.net>
2023-01-27 14:24:56 +0300

    Preparing to release picortex-sdk:0.01.68

--- core/src/commonMain/kotlin/viewmodel/LazyViewModel.kt
+++ core/src/commonMain/kotlin/viewmodel/LazyViewModel.kt
@@ -5,6 +5,23 @@ package viewmodel
 
 import kase.LazyState
 import kase.Pending
+import koncurrent.FailedLater
+import koncurrent.Later
 import kotlin.js.JsExport
 
-abstract class LazyViewModel<S>(config: ScopeConfig<Any?>) : ViewModel<LazyState<S>>(config.of(Pending))
+abstract class LazyViewModel<S>(config: ScopeConfig<Any?>) : ViewModel<LazyState<S>>(config.of(Pending)) {
+
+    protected fun <R> ifNotPending(block: () -> Later<R>): Later<R> {
+        if (ui.value !is Pending) return mustBePending()
+        return block()
+    }
+
+    open fun deInitialize() {
+        ui.value = Pending
+    }
+
+    private companion object {
+        const val MUST_BE_PENDING = "Scope must be in a pending state for it be initialized"
+        inline fun mustBePending() = FailedLater(IllegalStateException(MUST_BE_PENDING))
+    }
+}
